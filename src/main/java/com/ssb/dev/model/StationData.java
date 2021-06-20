2b185f22c495960357883f2f4a485ea46eaf2d5e
praveen gupta <praveen.vin28@gmail.com>
2021-06-20 17:27:09 +0200

    Remove comment

--- src/main/java/com/ssb/dev/model/StationData.java
+++ src/main/java/com/ssb/dev/model/StationData.java
@@ -5,22 +5,6 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-/*
-{
-  "last_updated": 1540219230,
-  "data": {
-    "stations": [
-      {
-        "is_installed": 1,
-        "is_renting": 1,
-        "num_bikes_available": 7,
-        "num_docks_available": 5,
-        "last_reported": 1540219230,
-        "is_returning": 1,
-        "station_id": "175"
-      },
- */
-
 @Builder
 @Data
 @NoArgsConstructor
